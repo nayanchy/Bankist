8d794a65ef0be10f91ddfd3a62c81994ef8979e0
nayanchy <39651128+nayanchy@users.noreply.github.com>
2021-08-27 21:37:28 +0600

    Final Version of the Codes

--- script.js
+++ script.js
@@ -10,61 +10,64 @@ const account1 = {
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
-};
+}
 
 const account2 = {
   owner: 'Jessica Davis',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
-};
+}
 
 const account3 = {
   owner: 'Steven Thomas Williams',
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
-};
+}
 
 const account4 = {
   owner: 'Sarah Smith',
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
-};
+}
 
-const accounts = [account1, account2, account3, account4];
+const accounts = [account1, account2, account3, account4]
+
+let currentAccount
 
 // Elements
-const labelWelcome = document.querySelector('.welcome');
-const labelDate = document.querySelector('.date');
-const labelBalance = document.querySelector('.balance__value');
-const labelSumIn = document.querySelector('.summary__value--in');
-const labelSumOut = document.querySelector('.summary__value--out');
-const labelSumInterest = document.querySelector('.summary__value--interest');
-const labelTimer = document.querySelector('.timer');
+const labelWelcome = document.querySelector('.welcome')
+const labelDate = document.querySelector('.date')
+const labelBalance = document.querySelector('.balance__value')
+const labelSumIn = document.querySelector('.summary__value--in')
+const labelSumOut = document.querySelector('.summary__value--out')
+const labelSumInterest = document.querySelector('.summary__value--interest')
+const labelTimer = document.querySelector('.timer')
 
-const containerApp = document.querySelector('.app');
-const containerMovements = document.querySelector('.movements');
+const containerApp = document.querySelector('.app')
+const containerMovements = document.querySelector('.movements')
 
-const btnLogin = document.querySelector('.login__btn');
-const btnTransfer = document.querySelector('.form__btn--transfer');
-const btnLoan = document.querySelector('.form__btn--loan');
-const btnClose = document.querySelector('.form__btn--close');
-const btnSort = document.querySelector('.btn--sort');
+const btnLogin = document.querySelector('.login__btn')
+const btnTransfer = document.querySelector('.form__btn--transfer')
+const btnLoan = document.querySelector('.form__btn--loan')
+const btnClose = document.querySelector('.form__btn--close')
+const btnSort = document.querySelector('.btn--sort')
 
-const inputLoginUsername = document.querySelector('.login__input--user');
-const inputLoginPin = document.querySelector('.login__input--pin');
-const inputTransferTo = document.querySelector('.form__input--to');
-const inputTransferAmount = document.querySelector('.form__input--amount');
-const inputLoanAmount = document.querySelector('.form__input--loan-amount');
-const inputCloseUsername = document.querySelector('.form__input--user');
-const inputClosePin = document.querySelector('.form__input--pin');
+const inputLoginUsername = document.querySelector('.login__input--user')
+const inputLoginPin = document.querySelector('.login__input--pin')
+const inputTransferTo = document.querySelector('.form__input--to')
+const inputTransferAmount = document.querySelector('.form__input--amount')
+const inputLoanAmount = document.querySelector('.form__input--loan-amount')
+const inputCloseUsername = document.querySelector('.form__input--user')
+const inputClosePin = document.querySelector('.form__input--pin')
 
-const displayMovements = function (movements){
+const displayMovements = function (account, sort = false){
+  const movs = sort ? account.movements.slice().sort((a,b) => a-b) : account.movements
   containerMovements.innerHTML = ''
-  movements.forEach(function(mov, i){
-    const type = mov > 0 ? 'deposit' : 'withdrawal';
+  movs.forEach(function(mov, i){
+    const type = mov > 0 ? 'deposit' : 'withdrawal'
 
     const html = `
     <div class="movements__row">
@@ -88,9 +91,9 @@ const generateUserName = function(accs){
 }
 generateUserName(accounts)
 
-const calcPrintBalance = function(movements){
-  const balance =  movements.reduce((acc, cur)=>acc + cur,0)
-
+const calcPrintBalance = function(account){
+  const balance =  account.movements.reduce((acc, cur)=>acc + cur,0)
+  account.balance = balance
   labelBalance.textContent = `${balance}€`
 }
 
@@ -117,10 +120,20 @@ const calcDisplaySummary = function(account){
   labelSumInterest.textContent = `${Math.abs(interest)}€`
 }
 
+const displayDetails = function (account){
+   
+    // Display movements
+    displayMovements(account)
+
+    // Display Balance
+    calcPrintBalance(account)
+
+    // Display Summary
+    calcDisplaySummary(account)
+}
 
 
-let currentAccount
-
+// Login
 btnLogin.addEventListener('click', function(e){
   e.preventDefault()
 
@@ -136,117 +149,80 @@ btnLogin.addEventListener('click', function(e){
     inputLoginUsername.value = inputLoginPin.value = ''
     inputLoginPin.blur()
     
-    // Display movements
-    displayMovements(currentAccount.movements)
-
-    // Display Balance
-    calcPrintBalance(currentAccount.movements)
-
-    // Display Summary
-    calcDisplaySummary(currentAccount)
+    displayDetails(currentAccount)
   }
 
 
 })
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
+//Transfer
+btnTransfer.addEventListener('click', function(e){
+  e.preventDefault()
+  
+  const transferTo = accounts.find(acc => acc.username === inputTransferTo.value)
+  const transferAmount = Number(inputTransferAmount.value)
+  const currBalance = currentAccount.balance
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+  if(transferTo && transferAmount < currBalance && transferAmount > 0 && transferTo?.username != currentAccount?.username){
+    transferTo?.movements.push(transferAmount)
+    currentAccount.movements.push(-Math.abs(transferAmount))
+    inputTransferTo.value= ""
+    inputTransferAmount.value= ""
+    inputTransferAmount.blur()
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+    displayDetails(currentAccount)
+  }
+  
+})
 
-/**
- * Find Method
- */
-// const firstWithdrawal = movements.find(mov => mov < 0)
+// Loan
 
-// const account = accounts.find(acc => acc.owner === 'Jessica Davis')
-// console.log(account)
+btnLoan.addEventListener('click', function(e){
+  e.preventDefault()
 
+  const loanAmount = Number(inputLoanAmount.value)
 
+  if(currentAccount.movements.some(mov => mov >= loanAmount*0.1) && loanAmount > 0){
+    currentAccount.movements.push(loanAmount)
+    displayDetails(currentAccount)
 
+    inputLoanAmount.value = ""
+    inputLoanAmount.blur()
+  }
+})
 
-/**
- * Map Method
- */
+// Close Account
 
-// const movement = [100, 234, -445, 3234, -543, -541, 400];
+btnClose.addEventListener('click', function(e){
+  e.preventDefault()
 
-// const eurToUsd = 1.1;
-// const movementsNew = movement.map(function(move){
-//   return Math.floor(move * eurToUsd);
-// })
+  const closeUser = inputCloseUsername.value
+  const closePin = Number(inputClosePin.value)
 
-// console.log(movementsNew)
+  if(closeUser === currentAccount.username && closePin === currentAccount.pin){
+    const closeAccIndex = accounts.findIndex(acc => acc.username === closeUser)
+    accounts.splice(closeAccIndex, 1)
 
-// const movementsNew1 = movement.map((move)=> Math.floor(move * eurToUsd));
+    containerApp.style.opacity = 0
 
-// console.log(movementsNew1)
+    inputClosePin.value = ""
+    inputCloseUsername.value = ""
+    inputCloseUsername.blur()
+  }
+})
 
-/**
- * Filter Method
- */
+// Sort Movements
+let clickCount=0;
+btnSort.addEventListener('click', function (e){
+  e.preventDefault()
+  
+  clickCount++
 
-// const deposits = movements.filter(mov => mov > 0)
+  if(clickCount%2 == 1){
+    displayMovements(currentAccount, true)
+  }else{
+    displayMovements(currentAccount, false)
+  }
 
-// console.log(deposits)
-
-/**
- * Reduce Method
- */
-// Getting the max movement by reduce method
-// const max = account1.movements.reduce((acc,curr) => acc > curr ? acc : curr, movements[0])
-
-// console.log(max)
-
-///////////////////////////////////////
-// Coding Challenge #2
-
-/* 
-Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
-Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
-1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4.
-2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
-3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
-4. Run the function for both test datasets
-TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
-TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
-GOOD LUCK 😀
-*/
-// Solution 01
-// const data1 = [5, 2, 4, 1, 15, 8, 3]
-// const data2 = [16, 6, 10, 5, 6, 1, 4]
-
-// let data1humAge = []
-
-// // With foreach method
-
-// // const calcAverageHumanAge = function (dogAge){
-// //   dogAge.forEach(function(age){
-// //     let humanage = 0;
-// //     age <=2 ? humanage = 2*age : humanage = 16 + age*4;
-// //     data1humAge.push(humanage)
-// //   })
-// // }
-
-// // With Map method
-
-// const calcAverageHumanAge = function(dogAge){
-//   let ageSum = 0
-//   const humanAge = dogAge.map(age => age <=2 ? 2*age : 16 + age*4).filter(age => age > 18)
-//   // humanAge.forEach(function(ags){
-//   //   ageSum += ags
-//   // })
-
-//   const avg = humanAge.reduce((acc, age) => acc + age)/humanAge.length
-
-//   console.log(avg)
-// }
-// calcAverageHumanAge(data2)
+})
 
